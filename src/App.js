cc7db58a125ed3c5491abec88f5df292294ec8db
n1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-10-12 10:55:46 +0200

    styles: Added dark mode as default style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
+import { AppRouter } from './routers/AppRouter';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+	const theme = useMemo(() => createMuiTheme({
+		palette: { type: prefersDarkMode ? 'dark' : 'light', },
+	}), [prefersDarkMode])
+
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline/>
+			<Provider store={ store }>
+				<AppRouter />
+			</Provider>
+		</ThemeProvider>
+	);
 }
 
 export default App;
