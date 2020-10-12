58be5dcf526c145f9bc7c93054ac87a85ab0e120
n1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-10-12 10:52:28 +0200

    feat: Added new actions and a reducer to manage UI actions

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,4 +1,41 @@
+import { firebase, googleAuthProvider } from "../../firebase/firebaseConfig";
 import { types } from "../types/types";
+import { startLoading, finishLoading } from './ui'
+
+export const startLoginEmailPassword = (email, password) => {
+    return (dispatch) => {
+        dispatch(startLoading());
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then( ({ user }) => {
+                dispatch(login(user.uid, user.displayName));
+                dispatch(finishLoading());
+            })
+            .catch( e => {
+                dispatch(finishLoading());
+                console.log(e.message);
+            })
+    }
+};
+
+export const startRegisterWithEmailPasswordName = (email, password, name) => {
+    return (dispatch) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then( async({ user }) => {
+                await user.updateProfile({ displayName: name });
+                dispatch(login(user.uid, user.displayName));
+            })
+            .catch( e => {
+                console.log(e);
+            })
+    }
+};
+
+export const startGoogleLogin = () => {
+    return (dispatch) => {
+        firebase.auth().signInWithPopup(googleAuthProvider)
+            .then( ({ user }) => dispatch(login(user.uid, user.displayName)));
+    }
+};
 
 export const login = (uid, displayName) => ({
     type: types.login,
@@ -7,6 +44,13 @@ export const login = (uid, displayName) => ({
         displayName
     }
 });
+
+export const startLogout = () => {
+    return async (dispatch) => {
+        await firebase.auth().signOut();
+        dispatch(logout());
+    }
+};
 
 export const logout = () => ({
     type: types.logout
