fd9c108b3ff58cc583e139a1a4dcf07cfc589f9e
n1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-10-12 10:49:02 +0200

    feat: Added a custom hook to manage useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export const useForm = ( initialState = {} ) => {
+    
+    const [values, setValues] = useState(initialState);
+    const reset = (newFormState = initialState) => setValues( newFormState );
+    const handleInputChange = (e) => setValues( { ...values, [ e.target.name ]: e.target.value } );
+
+    return [ values, setValues, handleInputChange, reset ];
+}
